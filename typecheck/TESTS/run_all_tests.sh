576696374628e832bcd52469102372475d740a5c
ivel <ivel@pop-os.localdomain>
2022-11-28 15:34:13 -0500

    typecheck tests

--- typecheck/TESTS/run_all_tests.sh
+++ typecheck/TESTS/run_all_tests.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 echo "**********RESOLVE**********\n\n"
+
 echo "\n\n\nOnline Examples"
 
 echo "good files"
@@ -23,32 +24,6 @@ do
 			echo "$testfile 1st pass success (INCORRECT)"
 		else
 			echo "$testfile 1st pass failure (as expected)"
-	fi
-done
-
-echo "\n\n\nTHESE ARE MY TEST CASES"
-
-echo "good files"
-for testfile in mygood*.bminor
-do
-	if  ./bminor -resolve  $testfile > $testfile.out
-	then
-			echo "$testfile success (as expected)"
-	else
-		echo "$testfile 1st pass failure (INCORRECT)"
-	fi
-	
-done
-
-echo "bad files"
-
-for testfile in mybad*.bminor
-do
-	if ./bminor -resolve  $testfile > $testfile.out
-	then
-		echo "$testfile 1st pass success (INCORRECT)"
-	else
-			echo "$testfile 2nd pass failure (as expected)"
 	fi
 done
 
@@ -77,33 +52,5 @@ do
 			echo "$testfile 1st pass failure (as expected)"
 	fi
 done
-
-echo "\n\n\nTHESE ARE MY TEST CASES"
-
-echo "good files"
-for testfile in mygood*.bminor
-do
-	if  ./bminor -typecheck  $testfile > $testfile.out
-	then
-			echo "$testfile success (as expected)"
-	else
-		echo "$testfile 1st pass failure (INCORRECT)"
-	fi
-	
-done
-
-echo "bad files"
-
-for testfile in mybad*.bminor
-do
-	if ./bminor -typecheck  $testfile > $testfile.out
-	then
-		echo "$testfile 1st pass success (INCORRECT)"
-	else
-			echo "$testfile 2nd pass failure (as expected)"
-	fi
-done
-
-
 
 
